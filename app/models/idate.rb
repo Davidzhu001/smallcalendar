a94c35b7e3ac912dd6ef14b9d07a1fc1e5a6a135
Davidzhu001 <qluoluo@icloud.com>
2015-07-28 16:07:02 -0500

    rspec

--- app/models/idate.rb
+++ app/models/idate.rb
@@ -4,8 +4,8 @@ class Idate < ActiveRecord::Base
 	def self.between(start_time)
       where('starts_at > :start_date and starts_at < :ending_date')
     end
-    def self.months
-    	date_arrray = Idate.where('start_date <= ? OR ending_date >= ?', Date.today.month,  Date.today.month)
+    def self.months(date=Date.today)
+    	date_arrray = Idate.where('start_date <= ? OR ending_date >= ?', date,  date)
     	date_arrray.sort_by &:start_date
     end
     def self.dating(date)
